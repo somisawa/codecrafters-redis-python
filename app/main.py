2be3f4f1d2d2d44f9b1b5a29da82c9ad80c28c7f
somisawa <sota.misawa@akariinc.co.jp>
2023-01-05 23:07:59 +0900

    second push

--- app/main.py
+++ app/main.py
@@ -9,7 +9,8 @@ def main():
     # Uncomment this to pass the first stage
     #
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    server_socket.accept() # wait for client
+    conn, addr = server_socket.accept() # wait for client
+    conn.send("+PONG\r\n")
 
 
 if __name__ == "__main__":
